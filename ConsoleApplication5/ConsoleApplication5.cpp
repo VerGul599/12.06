73fd11f1feb9a6a3bb25cf4cccc23050b8895247
Вероника <@WIN-BBNSI5FE1UN>
2024-06-25 14:58:22 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+void plus(int x, int y)
+{
+    printf("x+y=%d \n", x + y);
+}
+
+void munys(int x, int y)
+{
+    printf("x-y=%d \n", x - y);
+}
+
+void ymnojenie(int x, int y)
+{
+    printf("x*y=%d \n", x * y);
+}
+
+int main(void)
+{
+    int a = 2;
+    int b = 10;
+
+    //тип(*имя_массива[размер]) (параметры)
+    void (*prosto[3])(int, int) = { plus, munys, ymnojenie };
+
+    int length = sizeof(prosto) / sizeof(prosto[0]);
+
+    for (int i = 0; i < length; i++)
+    {
+        prosto[i](a, b);  
+    }
+    return 0;
+}
+
